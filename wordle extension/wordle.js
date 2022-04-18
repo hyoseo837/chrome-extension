8876e90824b9e03f2dcf0a77d1f7597f5f1bc6de
Ugly_Potato <71345965+hyoseo837@users.noreply.github.com>
2022-04-18 09:00:35 +0200

    a1

--- wordle extension/wordle.js
+++ wordle extension/wordle.js
@@ -1,1 +1,13 @@
-var letter = document.getElementById('char2');
+var letters = [];
+letters.push(document.getElementById('char1'))
+letters.push(document.getElementById('char2'))
+letters.push(document.getElementById('char3'))
+letters.push(document.getElementById('char4'))
+letters.push(document.getElementById('char5'))
+
+for (let index = 0; index < letters.length; index++) {
+    const element = letters[index];
+    element.innerHTML = 'L'
+    console.log(element)
+    
+}
